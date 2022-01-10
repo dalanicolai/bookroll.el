54f685655f49eb3c20c27543d8f57110b1e76ea5
Daniel Nicolai <dalanicolai@gmail.com>
2022-01-10 03:05:18 +0100

    Implement support for pdf-tools

--- bookroll.el
+++ bookroll.el
@@ -30,6 +30,11 @@
 (defgroup bookroll nil
   "Bookroll sutomizations.")
 
+(defvar-local bookroll-mode-winprops-alist t
+  "Alist of windows to window properties.
+Each element has the form (WINDOW . ALIST).
+See `bookroll-mode-winprops'.")
+
 (defcustom br-scroll-fraction 4
   "Set the scroll step size in 1/fraction of page.")
 
@@ -37,9 +42,9 @@
 (defvar-local split-point-offset 1)
 
 (defvar-local overlays nil)
-(defvar-local page-sizes nil)
+(defvar-local image-sizes nil)
 ;; (defvar-local image-sizes nil)
-(defvar-local page-positions nil)
+(defvar-local image-positions nil)
 ;; (defvar-local image-positions nil)
 (defvar-local number-of-pages 0)
 
@@ -71,14 +76,14 @@
   (let ((inhibit-read-only t))
     (setq cursor-type nil)
 
-    (setq page-sizes (make-list 1000 '(800 . 1600)))
-    (setq page-positions (let ((sum 0)
+    (setq image-sizes (make-list 1000 '(800 . 1600)))
+    (setq image-positions (let ((sum 0)
                                positions)
-                           (dolist (s page-sizes)
+                           (dolist (s image-sizes)
                              (push sum positions)
                              (setq sum (+ sum (cdr s))))
                            (nreverse positions)))
-    (setq number-of-pages (length page-sizes))
+    (setq number-of-pages (length image-sizes))
 
     (dotimes (i 1000)
       (insert (number-to-string i))
@@ -92,21 +97,96 @@
 
 ;;; Code
 
+(defun bookroll-mode-winprops (&optional window cleanup)
+  "Return winprops of WINDOW.
+A winprops object has the shape (WINDOW . ALIST).
+WINDOW defaults to `selected-window' if it displays the current buffer, and
+otherwise it defaults to t, used for times when the buffer is not displayed."
+  (cond ((null window)
+         (setq window
+               (if (eq (current-buffer) (window-buffer)) (selected-window) t)))
+        ((eq window t))
+	      ((not (windowp window))
+	       (error "Not a window: %s" window)))
+  (when cleanup
+    (setq bookroll-mode-winprops-alist
+  	      (delq nil (mapcar (lambda (winprop)
+			                        (let ((w (car-safe winprop)))
+				                        (if (or (not (windowp w)) (window-live-p w))
+				                            winprop)))
+  			                    bookroll-mode-winprops-alist))))
+  (let ((winprops (assq window bookroll-mode-winprops-alist)))
+    ;; For new windows, set defaults from the latest.
+    (if winprops
+        ;; Move window to front.
+        (setq bookroll-mode-winprops-alist
+              (cons winprops (delq winprops bookroll-mode-winprops-alist)))
+      (setq winprops (cons window
+                           (copy-alist (cdar bookroll-mode-winprops-alist))))
+      ;; Add winprops before running the hook, to avoid inf-loops if the hook
+      ;; triggers window-configuration-change-hook.
+      (setq bookroll-mode-winprops-alist
+            (cons winprops bookroll-mode-winprops-alist))
+      (run-hook-with-args 'bookroll-mode-new-window-functions winprops))
+    winprops))
+
+(defun bookroll-mode-window-get (prop &optional winprops)
+  (declare (gv-setter (lambda (val)
+                        `(bookroll-mode-window-put ,prop ,val ,winprops))))
+  (unless (consp winprops) (setq winprops (bookroll-mode-winprops winprops)))
+  (cdr (assq prop (cdr winprops))))
+
+(defun bookroll-mode-window-put (prop val &optional winprops)
+  (unless (consp winprops) (setq winprops (bookroll-mode-winprops winprops)))
+  (unless (eq t (car winprops))
+    (bookroll-mode-window-put prop val t))
+  (setcdr winprops (cons (cons prop val)
+                         (delq (assq prop (cdr winprops)) (cdr winprops)))))
+
+(defun bookroll-set-window-vscroll (vscroll)
+  (setf (bookroll-mode-window-get 'vscroll) vscroll)
+  (set-window-vscroll (selected-window) vscroll t))
+
+(defun bookroll-set-window-hscroll (ncol)
+  (setf (bookroll-mode-window-get 'hscroll) ncol)
+  (set-window-hscroll (selected-window) ncol))
+
+(defun bookroll-mode-reapply-winprops ()
+  ;; When set-window-buffer, set hscroll and vscroll to what they were
+  ;; last time the image was displayed in this window.
+  (when (listp bookroll-mode-winprops-alist)
+    ;; Beware: this call to bookroll-mode-winprops can't be optimized away,
+    ;; because it not only gets the winprops data but sets it up if needed
+    ;; (e.g. it's used by doc-view to display the image in a new window).
+    (let* ((winprops (bookroll-mode-winprops nil t))
+           (hscroll (bookroll-mode-window-get 'hscroll winprops))
+           (vscroll (bookroll-mode-window-get 'vscroll winprops)))
+      (when (image-get-display-property) ;Only do it if we display an image!
+	      (if hscroll (set-window-hscroll (selected-window) hscroll))
+	      (if vscroll (set-window-vscroll (selected-window) vscroll t))))))
+
+(defun bookroll-mode-setup-winprops ()
+  ;; Record current scroll settings.
+  (unless (listp bookroll-mode-winprops-alist)
+    (setq bookroll-mode-winprops-alist nil))
+  (add-hook 'window-configuration-change-hook
+	          #'bookroll-mode-reapply-winprops nil t))
+
 ;; TODO replace test sizes
-(setq page-sizes (make-list 100 '(800 . 1600)))
-(setq page-positions (let ((sum 0)
+(setq image-sizes (make-list 100 '(800 . 1600)))
+(setq image-positions (let ((sum 0)
                             positions)
-                        (dolist (s page-sizes)
+                        (dolist (s image-sizes)
                           (push sum positions)
                           (setq sum (+ sum (cdr s))))
                         (nreverse positions)))
-(setq number-of-pages (length page-sizes))
+(setq number-of-pages (length image-sizes))
 
 (defun br-image-size (&optional page)
-  (nth (- (or page (br-current-page)) 1) page-sizes))
+  (nth (- (or page (br-current-page)) 1) image-sizes))
 
 (defun br-image-position (&optional page)
-  (nth (- page 1) page-positions))
+  (nth (- page 1) image-positions))
 
 (defun br-create-empty-page (size)
   (pcase-let* ((`(,w . ,h) size))
@@ -124,13 +204,13 @@ Pass non-nil value for include-first when the buffer text starts with a match."
                                                                 t))))
       (setq beg (- (search-forward-regexp split-regexp nil t)
                    1)))
-    ;; We want to create an overlay for each page in the `page-sizes' list.
+    ;; We want to create an overlay for each page in the `image-sizes' list.
     ;; When include-first is non-nil then the list already contains a first
     ;; overlay. For both lists a last overlay is added after the loop to be sure
     ;; it extends to (point-max).
     (dolist (x (if include-first
-                   (cddr page-sizes)
-                 (cdr page-sizes)))
+                   (cddr image-sizes)
+                 (cdr image-sizes)))
       (search-forward-regexp split-regexp nil t)
       (push (make-overlay beg (- (point) 1 split-point-offset)) overlays)
       (setq beg (- (point) 1)))
@@ -140,35 +220,36 @@ Pass non-nil value for include-first when the buffer text starts with a match."
 ;; TODO replace test sizes
 (defun br-create-placeholders ()
   (let ((ph (br-create-empty-page '(800 . 1600)))
-        (constant-size (cl-every #'eql page-sizes (cdr page-sizes))))
-      (dotimes (i (length page-sizes))
+        (constant-size (cl-every #'eql image-sizes (cdr image-sizes))))
+      (dotimes (i (length image-sizes))
         (overlay-put (nth i overlays) 'display (if constant-size
                                                    ph
-                                                 (br-create-empty-page (nth i page-sizes)))))))
+                                                 (br-create-empty-page (nth i image-sizes)))))))
 
 (defun br-current-page ()
   (interactive)
   (let ((i 0)
         (cur-pos (window-vscroll nil t)))
-    (while (<= (nth (1+ i) page-positions) (+ cur-pos (/ (window-pixel-height) 2)))
+    (while (<= (nth (1+ i) image-positions) (+ cur-pos (/ (window-pixel-height) 2)))
       (setq i (1+ i)))
     (1+ i)))
-    ;; (while (<= (print (nth (1+ i) page-positions)) (print (+ cur-pos (/ (window-pixel-height) 2))))
+    ;; (while (<= (print (nth (1+ i) image-positions)) (print (+ cur-pos (/ (window-pixel-height) 2))))
     ;;   (setq i (1+ i)))
     ;; (print (1+ i))))
 
-(defun br-display-page (page)
+(defun br-display-page (page image)
   (let ((elt (- page 1)))
-    (overlay-put (nth elt overlays) 'display (pcase (% page 3)
-                                               (0 im1)
-                                               (1 im2)
-                                               (2 im3)))))
+    (overlay-put (nth elt overlays) 'display image)))
+                 ;; (pcase (% page 3)
+                 ;;                               (0 im1)
+                 ;;                               (1 im2)
+                 ;;                               (2 im3)))))
 
 (defun br-undisplay-page (page)
   (let ((elt (- page 1)))
     (overlay-put (nth elt overlays)
                  'display
-                  (br-create-empty-page (car page-sizes)))))
+                  (br-create-empty-page (car image-sizes)))))
 
 (defun br-update-page-triplet (page)
   (let ((display-pages (pcase page
@@ -179,16 +260,18 @@ Pass non-nil value for include-first when the buffer text starts with a match."
       (unless (member p display-pages)
         (br-undisplay-page p)))
     (dolist (p display-pages)
-      (br-display-page p))
+      ;; TODO separate pdf function from bookroll package
+      (br-display-page p (pdf-view-create-page p)))
     (setq currently-displayed-pages display-pages)))
 
 (defun br-goto-page (page)
   (interactive "n")
   (br-update-page-triplet page)
   (let* ((elt (- page 1)))
-    (set-window-vscroll nil (nth elt page-positions) t)))
+    (set-window-vscroll nil (nth elt image-positions) t)))
 
-(defun br-scroll-up ()
+;; (defun br-scroll-up ()
+(defun pdf-view-next-line-or-next-page ()
   (interactive)
   ;; because pages could have different heights, we calculate the step size on each scroll
   ;; TODO define constant scroll size if doc has single page height
@@ -198,7 +281,9 @@ Pass non-nil value for include-first when the buffer text starts with a match."
     (let ((current-page (print (br-current-page))))
         (br-update-page-triplet current-page))))
 
-(defun br-scroll-down ()
+;; TODO separate pdf functions from bookroll package
+;; (defun br-scroll-down ()
+(defun pdf-view-previous-line-or-previous-page ()
   (interactive)
   ;; because pages could have different heights, we calculate the step size on each scroll
   ;; TODO define constant scroll size if doc has single page height
@@ -208,12 +293,12 @@ Pass non-nil value for include-first when the buffer text starts with a match."
     (let ((current-page (print (br-current-page))))
       (br-update-page-triplet current-page))))
 
-;; TODO Change to minor mode
-(define-derived-mode bookroll-mode special-mode "BookRoll"
-  "This is a continuous scroll engine for rendering books.")
+(define-minor-mode bookroll-mode
+  "This is a continuous scroll engine for rendering books."
+  :keymap
+  '(("j" . pdf-view-next-line-or-next-page)
+    ("k" . pdf-view-previous-line-or-previous-page)))
 
-(cond ((boundp 'evil-version)
-       (evil-define-key 'motion bookroll-mode-map "j" 'br-scroll-up)
-       (evil-define-key 'motion bookroll-mode-map "k" 'br-scroll-down))
-      (t (define-key bookroll-mode-map "j" 'br-scroll-up)
-         (define-key bookroll-mode-map "k" 'br-scroll-down)))
+(when (boundp 'evil-version)
+  (evil-define-key 'normal bookroll-mode-map "j" 'br-scroll-up)
+  (evil-define-key 'normal bookroll-mode-map "k" 'br-scroll-down))
